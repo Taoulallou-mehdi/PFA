584db5bb9b5800aa658be77998d52e040efab392
oukili02 <saidrissi28@gmail.com>
2025-04-08 11:06:19 +0100

    c

---
+++ frontend/constant/Colors.jsx
@@ -0,0 +1,13 @@
+const Colors = {
+    GREEN: '#4CAF50',
+    WHITE: '#FFFFFF',
+    GRAY: '#808080',
+    TEXT_DARK: '#000000',
+    TEXT_GRAY: '#666666',
+    INPUT_BG: '#F5F5F5',
+    INPUT_BORDER: '#EAEAEA',
+    DIVIDER: '#EAEAEA',
+    BORDER_LIGHT: '#EAEAEA',
+  };
+  
+  export default Colors;
