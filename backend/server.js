5ae29d28a174e6ee442948416986c1c954f407de
Taoulallou-mehdi <e.taoulallou@esisa.ac.ma>
2025-04-11 00:55:21 +0100

    patch

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
-// server.js
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const dotenv = require('dotenv');
+const UserRoutes = require('./src/routes/UserRoutes');
+const PoubelleRoutes = require('./src/routes/PoubelleRoutes');
+const CollecteRoutes = require('./src/routes/CollecteRoutes');
+const TrajetRoutes = require('./src/routes/TrajetRoutes');
+const { errorHandler, notFoundHandler } = require('./src/middleware/errorHandler');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -28,6 +33,14 @@ mongoose.connect(MONGO_URI, {
 app.get('/', (req, res) => {
     res.send('API Gestion Déchets en cours de développement');
 });
+
+app.use('/api/users', UserRoutes);
+app.use('/api/poubelles', PoubelleRoutes);
+app.use('/api/collectes', CollecteRoutes);
+app.use('/api/trajets', TrajetRoutes);
+app.use(notFoundHandler);
+
+app.use(errorHandler);
 
 // Lancement du serveur
 app.listen(PORT, () => {
